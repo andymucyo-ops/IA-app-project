471c507a7f0b50bb05b0d417620b49b4a00f88a1
Andy <andymucyo@gmail.com>
2026-05-06 16:39:58 +0200

    basic UI layout + image input handling

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import numpy as np
 import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+from utils.image_io import load_from_sample, load_from_upload
+
+
 
 
 def main():
@@ -21,7 +27,28 @@ def main():
     #main area 
     left_column, right_column = st.columns(2)
     with left_column:
-        st.info("Original image placeholder")
+        # st.info("Original image placeholder")
+        if input_selector =="Upload file":
+            uploaded_image: UploadedFile = st.file_uploader(
+                    "Uploaded image",
+                    type=["jpg", "png"]
+                    )
+            if uploaded_image is not None:
+                processed_image: np.ndarray = load_from_upload(uploaded_image)
+                st.image(processed_image)
+        else:
+            sample_selector = st.selectbox(
+                    "Sample images",
+                    ("Building", "Goat with glasses")
+                    )
+            # st.info("Sample image placeholder")
+            match sample_selector:
+                case "Building":
+                    sample_image: np.ndarray = load_from_sample("Building.jpeg")
+                case "Goat with glasses":
+                    sample_image: np.ndarray = load_from_sample("goat.JPG")
+            st.image(sample_image)
+        
     with right_column:
         st.info("Processed output placeholder")
 
